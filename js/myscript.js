7e16b8392708226d848dd509fdf62d7f5b12cb3e
Lanner <mauriciolanner@gmail.com>
2020-05-06 16:25:09 -0300

    atualização da tabela e mlhorias na visualização

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,138 @@
+function gera_cor() {
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';
+
+    for (var i = 0; i < 6; i++) {
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
+}
+
+function americaDados(pais) {
+    var retorno
+    $.ajax({
+        url: "https://bigdata-api.fiocruz.br/numero/casos/pais/" + pais,
+        async: false,
+        success: function (dados) {
+            //console.log(dados);
+            var cor = gera_cor();
+            var caixa = `
+                        <div class="col-sm-3 card text-center">
+                            <h3 style="color: `+ cor + `;">` + pais + `</h3>
+                            <h6 id="total_casos">Total de mortos: <strong>`+ dados.total_mortes + `</strong></h6>
+                        </div>
+                        `;
+
+            var tempo = dados.detalhes_por_dia;
+
+            var datas = [];
+            var mortos = [];
+            var casos = [];
+
+            function preencheData(index) {
+                datas.push(index.date);
+            }
+            function preencheMortos(index) {
+                mortos.push(index.new_deaths);
+            }
+            function preencheCasos(index) {
+                casos.push(index.new_cases);
+            }
+
+            tempo.forEach(preencheData);
+            tempo.forEach(preencheMortos);
+            tempo.forEach(preencheCasos);
+
+            retorno = {
+                caixa: caixa,
+                datas: datas,
+                mortos: mortos,
+                casos: casos,
+                pais: pais,
+                cor: cor,
+            }
+
+        }
+    });
+    return retorno;
+}
+
+//função de preencimento das variáveis
+var vetorPaises = [
+    'Argentina',
+    'Bolívia',
+    'Brasil',
+    'Chile',
+    'Colômbia',
+    'Costa Rica',
+    'Cuba', 'El Salvador',
+    'Equador',
+    'Guatemala',
+    'Haiti',
+    'Honduras',
+    'México',
+    'Nicarágua',
+    'Panamá',
+    'Paraguai',
+    'Peru',
+    'República Dominicana',
+    'Uruguai',
+    'Venezuela'];
+var dados = [];
+function preencheDadosPaises(pais) {
+    var dadosPais = americaDados(pais);
+    dados.push({
+        caixa: dadosPais.caixa,
+        date: dadosPais.datas,
+        data: dadosPais.mortos,
+        label: dadosPais.pais,
+        borderColor: dadosPais.cor,
+        fill: false
+    })
+}
+
+function caixa(array) {
+    document.getElementById("cartoes").innerHTML += array.caixa
+}
+
+vetorPaises.forEach(preencheDadosPaises);
+
+dados.forEach(caixa)
+
+//preenchimento da tabela
+var ctx = document.getElementById("curvaMorte");
+var curvaMorte = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: dados[0].date,
+        datasets: dados
+    }
+});
+
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+            {
+                label: "Morte por país",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [2478, 5267, 734, 784, 433]
+            }
+        ]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
+        }
+    }
+});
+
+
+
+/*
 function atualizaDados(country) {
     $.ajax({
         url: "https://bigdata-api.fiocruz.br/numero/casos/pais/" + country.value,
@@ -15,6 +150,7 @@ function atualizaDados(country) {
             var datas = [];
             var mortos = [];
             var casos = [];
+
             function preencheData(index) {
                 datas.push(index.date);
             }
@@ -57,3 +193,4 @@ function atualizaDados(country) {
             $("#loader").html("Erro ao carregar os dados");
         });
 }
+*/
